9719a61a50b9c1b9cb451d16d392980eddebf255
kevin meyers <kevinm1776@gmail.com>
2020-08-17 12:53:59 -0400

    Added default text as the current data.

--- src/Handler/ColumnEdit.hs
+++ src/Handler/ColumnEdit.hs
@@ -11,33 +11,36 @@ import Import
 
 getColumnEditR :: TableId -> ColumnId -> Handler Html
 getColumnEditR tableId columnId = do
-    (widget, enctype) <- generateFormPost columnForm
     column <- runDB $ getJust columnId
     table <- runDB $ getJust tableId
+    (widget, enctype) <- generateFormPost $ columnForm $ Just column
     defaultLayout $ do
         setTitle . toHtml $ "Update column " <> columnName column
         $(widgetFile "column-edit")
     
 data ColumnData = ColumnData
-    { columnDataDescription :: Maybe Text
+    { columnDataName :: Text
+    , columnDataDescription :: Maybe Text
     , columnDataDatatype :: Maybe Datatype
     , columnDataExample :: Maybe Text
     }
   deriving Show
 
-columnForm :: Form ColumnData
-columnForm = renderDivs $ ColumnData
-    <$> aopt textField "Description" Nothing
+columnForm :: Maybe Column -> Form ColumnData
+columnForm column = renderDivs $ ColumnData
+    <$> areq textField "Name" (columnName <$> column)
+    <*> aopt textField "Description" (columnDescription <$> column)
     <*> pure Nothing -- aopt textField "Datatype (leave empty)" Nothing
-    <*> aopt textField "Example" Nothing
+    <*> aopt textField "Example" (columnExample <$> column)
 
 postColumnEditR :: TableId -> ColumnId -> Handler ()
 postColumnEditR tableId columnId = do
-    ((result, _), _) <- runFormPost columnForm
+    ((result, _), _) <- runFormPost $ columnForm Nothing
     case result of
         FormSuccess columnData -> do
             runDB $ update columnId
-                [ ColumnDescription =. columnDataDescription columnData
+                [ ColumnName =. columnDataName columnData
+                , ColumnDescription =. columnDataDescription columnData
                 , ColumnDatatype =. columnDataDatatype columnData
                 , ColumnExample =. columnDataExample columnData
                 ]
