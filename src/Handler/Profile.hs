2f2f60b7061d9eeaf0315279eda8a4b0b34deae3
kevin meyers <kevinm1776@gmail.com>
2020-08-20 14:02:43 -0400

    Changing profile to redirect to form.

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -7,9 +7,23 @@ module Handler.Profile where
 
 import Import
 
-getProfileR :: Handler Html
-getProfileR = do
-    (_, user) <- requireAuthPair
-    defaultLayout $ do
-        setTitle . toHtml $ userIdent user <> "'s User page"
-        $(widgetFile "profile")
+data ProfileData = ProfileData 
+    { pName :: Text
+    , bio :: Maybe Text
+    , photoUrl :: Maybe Text
+    , tName :: Text
+    , tId :: TeamId
+    }
+
+getProfileR :: ProfileId -> Handler Html
+getProfileR profileId = do
+    mprofile <- runDB $ get profileId
+    case mprofile of
+        Nothing -> do
+            setUltDest $ ProfileR profileId ProfileDetailR
+            redirect $ ProfileR profileId ProfileEditR
+        Just (Entity _ profile) -> do
+            team <- runDB $ getJust $ profileTeamId profile
+            defaultLayout $ do
+                setTitle . toHtml $ profileName profile <> "'s User page"
+                $(widgetFile "profile")
