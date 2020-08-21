34b4a14209e4497683237272451441b14515e25a
kevin meyers <kevinm1776@gmail.com>
2020-08-20 20:01:43 -0400

    Added conditional edit link.

--- src/Handler/ProfileDetail.hs
+++ src/Handler/ProfileDetail.hs
@@ -18,7 +18,12 @@ data ProfileData = ProfileData
 getProfileDetailR :: ProfileId -> Handler Html
 getProfileDetailR profileId = do
     profile <- runDB $ getJust profileId
+    muid <- maybeAuthId
+    let canEdit = case muid of
+            Nothing -> False
+            Just uid -> uid == profileUserId profile
+
     (Entity teamId team) <- runDB $ getJustEntity $ profileTeamId profile
     defaultLayout $ do
         setTitle . toHtml $ profileName profile <> "'s User page"
-        $(widgetFile "profile")
+        $(widgetFile "profile-detail")
