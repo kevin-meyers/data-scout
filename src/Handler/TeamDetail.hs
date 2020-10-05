d4111be19399d4acbf0da99dbd3603520fe7d6c5
kevin meyers <kevinm1776@gmail.com>
2020-10-04 21:47:27 -0400

    Removed esquelleto since query is simplified.

--- src/Handler/TeamDetail.hs
+++ src/Handler/TeamDetail.hs
@@ -13,19 +13,11 @@ module Handler.TeamDetail where
 
 import Import
 
-import qualified Database.Esqueleto as E
-import           Database.Esqueleto      ((^.))
-
 getTeamDetailR :: TeamId -> Handler Html
 getTeamDetailR teamId = do
     team <- runDB $ get404 teamId
     tables <- runDB $ selectList [TableTeamId ==. teamId] []
-    profiles <- runDB
-        $ E.select
-        $ E.from $ \(user `E.InnerJoin` profile) -> do
-            E.on $ user ^. UserId E.==. profile ^. ProfileUserId
-            E.where_ $ user ^. UserTeamId E.==. E.val (Just teamId)
-            return profile
+    profiles <- runDB $ selectList [ProfileTeamId ==. teamId] []
 
     defaultLayout $ do
         setTitle . toHtml $ teamName team <> "'s page"
