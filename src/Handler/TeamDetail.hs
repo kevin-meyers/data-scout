c19357f3788537ddf41f819eb6544abba93fc4b8
kevin meyers <kevinm1776@gmail.com>
2020-10-17 16:40:53 -0400

    Use esqueleto to get tables that the user can actually see on a team.

--- src/Handler/TeamDetail.hs
+++ src/Handler/TeamDetail.hs
@@ -13,10 +13,29 @@ module Handler.TeamDetail where
 
 import Import
 
+import qualified Database.Esqueleto as E
+import           Database.Esqueleto      ((^.))
+
+
 getTeamDetailR :: TeamId -> Handler Html
 getTeamDetailR teamId = do
+    uid <- requireAuthId
     team <- runDB $ get404 teamId
-    tables <- runDB $ selectList [TableTeamId ==. teamId] []
+    profileId <- do
+        mprofile <- runDB $ getBy $ UniqueProfile uid
+        maybe notFound (pure . entityKey) mprofile
+
+    tables <- runDB
+        $ E.select
+        $ E.from $ \(table `E.InnerJoin` permission) -> do
+            E.on $ table ^. TableId E.==. permission ^. PermissionTableId
+            E.where_ $ permission ^. PermissionProfileId E.==. E.val profileId
+            E.where_ $ table ^. TableTeamId E.==. E.val teamId
+            return
+                ( table ^. TableId
+                , table ^. TableName
+                )
+
     profiles <- runDB $ selectList [ProfileTeamId ==. teamId] []
 
     defaultLayout $ do
